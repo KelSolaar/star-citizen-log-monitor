39616b2321da3c75cd369a0528911107ea5d9a90
Thomas Mansencal <thomas.mansencal@gmail.com>
2025-04-13 21:47:12 +1200

    Implement support to fetch player organisation.

--- star_citizen_log_monitor.py
+++ star_citizen_log_monitor.py
@@ -1,7 +1,10 @@
 # /// script
 # dependencies = [
 #   "aiofiles>=24,<25",
+#   "aiohttp>=3,<4",
+#   "beautifulsoup4>=4,<5",
 #   "click>=8,<9",
+#   "pickledb>=1,<2",
 #   "pytz>=2025",
 #   "textual>=2,<3",
 #   "tzlocal>=5,<6",
@@ -18,15 +21,20 @@ with an emphasis put on extracting combat related data.
 
 import asyncio
 import re
+import time
 import traceback
 from datetime import datetime
 from functools import wraps
+from pathlib import Path
 from typing import Callable
 
 import aiofiles
+import aiohttp
 import click
 import pytz
 import tzlocal
+from bs4 import BeautifulSoup
+from pickledb import PickleDB
 from rich.console import Console
 from rich.highlighter import RegexHighlighter
 from rich.theme import Theme
@@ -40,7 +48,7 @@ __license__ = "BSD-3-Clause - https://opensource.org/licenses/BSD-3-Clause"
 __maintainer__ = "Thomas Mansencal"
 __status__ = "Production"
 
-__version__ = "0.3.0"
+__version__ = "0.4.0"
 
 __all__ = [
     "LOCAL_TIMEZONE",
@@ -49,6 +57,10 @@ __all__ = [
     "PATTERN_NOTICE",
     "THEME_DEFAULT",
     "catch_exception",
+    "fetch_page",
+    "CACHE_ORGANIZATIONS",
+    "TTL_ORGANIZATION",
+    "extract_organization_name",
     "Logger",
     "EventHighlighter",
     "beautify_timestamp",
@@ -58,8 +70,8 @@ __all__ = [
     "parse_event_on_client_connected",
     "parse_event_request_quit_lobby",
     "parse_event_actor_death",
-    "parse_vehicle_destruction",
-    "parse_requesting_transition_event",
+    "parse_event_vehicle_destruction",
+    "parse_event_requesting_transition",
     "parse_event_actor_state_corpse",
     "parse_event_actor_stall",
     "EVENT_PARSERS",
@@ -90,7 +102,7 @@ THEME_DEFAULT = Theme(
         "sclh.vehicledestruction": "bold orange1",
         "sclh.vehicle": "italic #FF69B4",
         "sclh.driver": "italic #FF69B4",
-        "sclh.causedby": "italic #DC143C",
+        "sclh.causer": "italic #DC143C",
         "sclh.cause": "italic #A52A2A",
         # Actor State Corpse
         "sclh.corpse": "bold #DC143C",
@@ -103,15 +115,67 @@ THEME_DEFAULT = Theme(
 
 def catch_exception(function: Callable) -> Callable:
     @wraps(function)
-    def wrapper(*args, **kwargs):
+    async def wrapper(*args, **kwargs):
         try:
-            return function(*args, **kwargs)
+            return await function(*args, **kwargs)
         except Exception:
             tb = traceback.format_exc()
 
             return f"Exception in {function.__name__}: {tb}"
 
     return wrapper
+
+
+async def fetch_page(session: aiohttp.ClientSession, url: str) -> str:
+    async with session.get(url) as response:
+        return await response.text()
+
+
+CACHE_ORGANIZATIONS = PickleDB(Path(__file__).parent / ".organizations")
+
+TTL_ORGANIZATION = 3600 * 24 * 72  # 3 Days
+
+_ENABLE_ORGANIZATION_FETCHING = True
+
+
+async def extract_organization_name(player: str) -> str | None:
+    if not _ENABLE_ORGANIZATION_FETCHING:
+        return None
+
+    if player in CACHE_ORGANIZATIONS.all():
+        data = CACHE_ORGANIZATIONS.get(player)
+
+        if time.time() < data["expiration"]:
+            return data["organization"]
+
+    url = f"https://robertsspaceindustries.com/en/citizens/{player}"
+    async with aiohttp.ClientSession() as session:
+        html_content = await fetch_page(session, url)
+        soup = BeautifulSoup(html_content, "html.parser")
+        sid = soup.find(text="Spectrum Identification (SID)")
+        if sid is None:
+            CACHE_ORGANIZATIONS.set(
+                player,
+                {"organization": None, "expiration": time.time() + TTL_ORGANIZATION},
+            )
+
+            CACHE_ORGANIZATIONS.save()
+
+            return None
+
+        organization = sid.find_next().text.strip()
+
+        CACHE_ORGANIZATIONS.set(
+            player,
+            {
+                "organization": organization,
+                "expiration": time.time() + TTL_ORGANIZATION,
+            },
+        )
+
+        CACHE_ORGANIZATIONS.save()
+
+        return organization
 
 
 class Logger(Log):
@@ -125,20 +189,26 @@ class EventHighlighter(RegexHighlighter):
         PATTERN_TIMESTAMP_BEAUTIFIED,
         r"(?P<classifier>Zone): (?P<zone>[\w_-]+),",
         r"(?P<classifier>Requester): (?P<requester>[\w_-]+)",
+        r"(?P<classifier>Requester): (?P<requester>[\w_-]+ \([\w_-]+\))",
         # Actor Death
         r"(?P<actordeath>\[Actor Death\])",
         r"(?P<classifier>Victim): (?P<victim>[\w_-]+),",
+        r"(?P<classifier>Victim): (?P<victim>[\w_-]+ \([\w_-]+\)),",
         r"(?P<classifier>Killer): (?P<killer>[\w_-]+),",
+        r"(?P<classifier>Killer): (?P<killer>[\w_-]+ \([\w_-]+\)),",
         r"(?P<classifier>Weapon): (?P<weapon>[\w_-]+)",
         # Vehicle Destruction
         r"(?P<vehicledestruction>\[Vehicle Destruction\])",
         r"(?P<classifier>Vehicle): (?P<vehicle>[\w_-]+),",
         r"(?P<classifier>Driver): (?P<driver>[\w_-]+),",
-        r"(?P<classifier>Caused By): (?P<causedby>[\w_-]+),",
+        r"(?P<classifier>Driver): (?P<driver>[\w_-]+ \([\w_-]+\)),",
+        r"(?P<classifier>Caused By): (?P<causer>[\w_-]+),",
+        r"(?P<classifier>Caused By): (?P<causer>[\w_-]+ \([\w_-]+\)),",
         r"(?P<classifier>Cause): (?P<cause>[\w_-]+),",
         # Actor State Corpse
         r"(?P<corpse>\[Corpse\])",
-        r"(?P<classifier>Player): (?P<player>[\w_-]+),",
+        r"(?P<classifier>Player): (?P<player>[\w_-]+)",
+        r"(?P<classifier>Player): (?P<player>[\w_-]+ \([\w_-]+\))",
         # Actor Stall
         r"(?P<actorstall>\[Actor Stall\])",
     ]
@@ -161,7 +231,7 @@ def beautify_entity_name(name: str) -> str:
 
 
 @catch_exception
-def parse_event_on_client_spawned(log_line: str) -> str:
+async def parse_event_on_client_spawned(log_line: str) -> str:
     pattern = re.compile(
         "<"
         + PATTERN_TIMESTAMP_RAW
@@ -178,7 +248,7 @@ def parse_event_on_client_spawned(log_line: str) -> str:
 
 
 @catch_exception
-def parse_event_connect_started(log_line: str) -> str:
+async def parse_event_connect_started(log_line: str) -> str:
     pattern = re.compile(
         "<"
         + PATTERN_TIMESTAMP_RAW
@@ -195,7 +265,7 @@ def parse_event_connect_started(log_line: str) -> str:
 
 
 @catch_exception
-def parse_event_on_client_connected(log_line: str) -> str:
+async def parse_event_on_client_connected(log_line: str) -> str:
     pattern = re.compile(
         "<"
         + PATTERN_TIMESTAMP_RAW
@@ -212,7 +282,7 @@ def parse_event_on_client_connected(log_line: str) -> str:
 
 
 @catch_exception
-def parse_event_request_quit_lobby(log_line: str) -> str:
+async def parse_event_request_quit_lobby(log_line: str) -> str:
     pattern = re.compile(
         PATTERN_NOTICE
         + r"<\[EALobby\] EALobbyQuit> \[EALobby\]\[CEALobby::RequestQuitLobby\] (?P<requester>[\w_-]+) Requesting QuitLobby"
@@ -221,13 +291,17 @@ def parse_event_request_quit_lobby(log_line: str) -> str:
     if search := pattern.search(log_line):
         data = search.groupdict()
 
-        return f"{beautify_timestamp(data['timestamp'])} [Request Quit Lobby] Requester: {data['requester']}"
+        requester = beautify_entity_name(data["requester"])
+        if organization := await extract_organization_name(requester):
+            requester = f"{requester} ({organization})"
+
+        return f"{beautify_timestamp(data['timestamp'])} [Request Quit Lobby] Requester: {requester}"
 
     return None
 
 
 @catch_exception
-def parse_event_actor_death(log_line: str) -> str:
+async def parse_event_actor_death(log_line: str) -> str:
     pattern = re.compile(
         PATTERN_NOTICE
         + r"<Actor Death> CActor::Kill: '(?P<victim>[\w_-]+)' \[\d+\] in zone '(?P<zone>[\w_-]+)' killed by '(?P<killer>[\w_-]+)' \[\d+\] using '(?P<weapon>[\w_-]+)' \[Class (?P<weapon_class>[\w_-]+)\] with damage type '(?P<damage_type>[\w_-]+)' from direction x: (?P<direction_x>[-\d.]+), y: (?P<direction_y>[-\d.]+), z: (?P<direction_z>[-\d.]+)"
@@ -236,10 +310,18 @@ def parse_event_actor_death(log_line: str) -> str:
     if search := pattern.search(log_line):
         data = search.groupdict()
 
+        victim = beautify_entity_name(data["victim"])
+        if organization := await extract_organization_name(victim):
+            victim = f"{victim} ({organization})"
+
+        killer = beautify_entity_name(data["killer"])
+        if organization := await extract_organization_name(killer):
+            killer = f"{killer} ({organization})"
+
         return (
             f"{beautify_timestamp(data['timestamp'])} [Actor Death] "
-            f"Victim: {beautify_entity_name(data['victim'])}, "
-            f"Killer: {beautify_entity_name(data['killer'])}, "
+            f"Victim: {victim}, "
+            f"Killer: {killer}, "
             f"Zone: {beautify_entity_name(data['zone'])}, "
             f"Weapon: {beautify_entity_name(data['weapon'])}"
         )
@@ -248,7 +330,7 @@ def parse_event_actor_death(log_line: str) -> str:
 
 
 @catch_exception
-def parse_vehicle_destruction(log_line: str) -> str:
+async def parse_event_vehicle_destruction(log_line: str) -> str:
     pattern = re.compile(
         PATTERN_NOTICE
         + r"<Vehicle Destruction> CVehicle::OnAdvanceDestroyLevel: Vehicle '(?P<vehicle_name>[\w_-]+)' \[(?P<vehicle_id>\d+)\] in zone '(?P<zone>[\w_-]+)' \[pos x: (?P<pos_x>[-\d.]+), y: (?P<pos_y>[-\d.]+), z: (?P<pos_z>[-\d.]+) vel x: (?P<vel_x>[-\d.]+), y: (?P<vel_y>[-\d.]+), z: (?P<vel_z>[-\d.]+)\] driven by '(?P<driver>[\w_-]+)' \[(?P<driver_id>\d+)\] advanced from destroy level (?P<destroy_level_from>\d+) to (?P<destroy_level_to>\d+) caused by '(?P<causer>[\w_-]+)' \[(?P<causer_id>\d+)\] with '(?P<cause>[^\']+)'"
@@ -257,11 +339,19 @@ def parse_vehicle_destruction(log_line: str) -> str:
     if search := pattern.search(log_line):
         data = search.groupdict()
 
+        driver = beautify_entity_name(data["driver"])
+        if organization := await extract_organization_name(driver):
+            driver = f"{driver} ({organization})"
+
+        causer = beautify_entity_name(data["causer"])
+        if organization := await extract_organization_name(causer):
+            causer = f"{causer} ({organization})"
+
         return (
             f"{beautify_timestamp(data['timestamp'])} [Vehicle Destruction] "
             f"Vehicle: {beautify_entity_name(data['vehicle_name'])}, "
-            f"Driver: {beautify_entity_name(data['driver'])}, "
-            f"Caused By: {beautify_entity_name(data['causer'])}, "
+            f"Driver: {driver}, "
+            f"Caused By: {causer}, "
             f"Cause: {data['cause']}, "
             f"Zone: {beautify_entity_name(data['zone'])}"
         )
@@ -270,7 +360,7 @@ def parse_vehicle_destruction(log_line: str) -> str:
 
 
 @catch_exception
-def parse_requesting_transition_event(log_line: str) -> str:
+async def parse_event_requesting_transition(log_line: str) -> str:
     pattern = re.compile(
         PATTERN_NOTICE
         + r"<Requesting Transition> (?P<component>[^\|]+) \| (?P<status>[^\|]+) \| (?P<current_system>[^\|]+) \| (?P<host>[\w_-]+) \[(?P<host_id>\d+)\] \| Transitioning from zone (?P<from_zone>[\w_-]+) in (?P<from_system>[\w_-]+) to zone (?P<to_zone>[\w_-]+) in (?P<to_system>[\w_-]+) \[(?P<team>[\w_-]+)\]\[(?P<category>\w+)\]"
@@ -289,7 +379,7 @@ def parse_requesting_transition_event(log_line: str) -> str:
 
 
 @catch_exception
-def parse_event_actor_state_corpse(log_line: str) -> str:
+async def parse_event_actor_state_corpse(log_line: str) -> str:
     pattern = re.compile(
         PATTERN_NOTICE
         + r"<\[ActorState\] Corpse> \[ACTOR STATE\]\[SSCActorStateCVars::LogCorpse\] Player '(?P<player>[\w_-]+)' <remote client>: Running corpsify for corpse. \[Team_ActorTech\]\[Actor\]"
@@ -298,15 +388,17 @@ def parse_event_actor_state_corpse(log_line: str) -> str:
     if search := pattern.search(log_line):
         data = search.groupdict()
 
-        return (
-            f"{beautify_timestamp(data['timestamp'])} [Corpse] "
-            f"Player: {beautify_entity_name(data['player'])}, "
-        )
+        player = beautify_entity_name(data["player"])
+        if organization := await extract_organization_name(player):
+            player = f"{player} ({organization})"
+
+        return f"{beautify_timestamp(data['timestamp'])} [Corpse] Player: {player}"
 
     return None
 
+
 @catch_exception
-def parse_event_actor_stall(log_line: str) -> str:
+async def parse_event_actor_stall(log_line: str) -> str:
     pattern = re.compile(
         r"<(?P<timestamp>[\d\-T:.Z]+)> \[Notice\] <Actor stall> Actor stall detected, Player: (?P<player>[\w_-]+), Type: (?P<type>\w+), Length: (?P<length>[\d.]+)\. \[Team_ActorTech\]\[Actor\]"
     )
@@ -314,12 +406,14 @@ def parse_event_actor_stall(log_line: str) -> str:
     if search := pattern.search(log_line):
         data = search.groupdict()
 
-        return (
-            f"{beautify_timestamp(data['timestamp'])} [Actor Stall] "
-            f"Player: {beautify_entity_name(data['player'])}, "
-        )
+        player = beautify_entity_name(data["player"])
+        if organization := await extract_organization_name(player):
+            player = f"{player} ({organization})"
+
+        return f"{beautify_timestamp(data['timestamp'])} [Actor Stall] Player: {player}"
 
     return None
+
 
 EVENT_PARSERS = [
     parse_event_on_client_spawned,
@@ -327,8 +421,8 @@ EVENT_PARSERS = [
     parse_event_on_client_connected,
     parse_event_request_quit_lobby,
     parse_event_actor_death,
-    parse_vehicle_destruction,
-    parse_requesting_transition_event,
+    parse_event_vehicle_destruction,
+    parse_event_requesting_transition,
     parse_event_actor_state_corpse,
     parse_event_actor_stall,
 ]
@@ -354,9 +448,9 @@ class StarCitizenLogMonitorApp(App):
         self.logger.write_line(f"[ {self.__class__.__name__} - {__version__} ]")
         self.worker = self.run_worker(self.monitor())
 
-    def process_line(self, line: str):
+    async def process_line(self, line: str):
         for event_parser in EVENT_PARSERS:
-            if parsed_event := event_parser(line):
+            if parsed_event := await event_parser(line):
                 self.logger.write_line(parsed_event)
 
     async def monitor(self):
@@ -365,7 +459,7 @@ class StarCitizenLogMonitorApp(App):
                 lines = await file.readlines()
 
                 for line in lines:
-                    self.process_line(line)
+                    await self.process_line(line)
 
                 while True:
                     line = await file.readline()
@@ -375,10 +469,10 @@ class StarCitizenLogMonitorApp(App):
 
                         continue
 
-                    self.process_line(line)
+                    await self.process_line(line)
 
         except Exception as error:
-            self.process_line(str(error))
+            await self.process_line(str(error))
 
 
 @click.command()
@@ -387,7 +481,16 @@ class StarCitizenLogMonitorApp(App):
     default=r"C:\Program Files\Roberts Space Industries\StarCitizen\LIVE\Game.log",
     help="Log file path",
 )
-def main(log_file_path: str) -> None:
+@click.option(
+    "--enable-organization-fetching",
+    default=True,
+    help="Whether to enable player organization fetching",
+)
+def main(log_file_path: str, enable_organization_fetching: bool) -> None:
+    global _ENABLE_ORGANIZATION_FETCHING
+
+    _ENABLE_ORGANIZATION_FETCHING = enable_organization_fetching
+
     app = StarCitizenLogMonitorApp(log_file_path)
     app.run()
 
