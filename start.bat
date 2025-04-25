15ab27ec15bdda17aa0d07464aa509fd2eb42e4f
Thomas Mansencal <thomas.mansencal@gmail.com>
2025-04-25 18:02:12 +1200

    Update start.bat file.

--- start.bat
+++ start.bat
@@ -1,11 +1,14 @@
-Rem Paste your path to the star citizen LIVE game directory (replace this with the PTU if needed)
-set SC_DIR=C:\Program Files\Roberts Space Industries\StarCitizen\LIVE
-set UV_PATH=%HOMEDRIVE%%HOMEPATH%\.local\bin\uv
+REM Paste your path to the Star Citizen LIVE game directory (replace with the PTU as required).
+set SC_DIR="C:\Program Files\Roberts Space Industries\StarCitizen\LIVE"
+set UV_PATH="%HOMEDRIVE%%HOMEPATH%\.local\bin\uv"
 
-Rem Ensure uv is installed
+REM Ensure that `uv` is installed.
 if not exist "%UV_PATH%" (
     powershell -ExecutionPolicy ByPass -c "irm https://astral.sh/uv/install.ps1 | iex"
 )
 
-Rem run it!
-%UV_PATH% run ./star_citizen_log_monitor.py --log-file-path="%SC_DIR%\Game.log"
+REM Run it!
+%UV_PATH% run ./star_citizen_log_monitor.py --log-file-path=%SC_DIR%\Game.log
+
+REM Alternative launch from Github:
+REM %UV_PATH% run https://raw.githubusercontent.com/KelSolaar/star-citizen-log-monitor/refs/heads/master/star_citizen_log_monitor.py --log-file-path=%SC_DIR%\Game.log
